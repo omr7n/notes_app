9b4a4df1fc8ce56033cc36aa79b25e5b547f6f94
omran abo ali <omranza12@gmail.com>
2024-03-23 03:11:32 +0200

    create type adabter for note model

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,9 +1,21 @@
-class NoteModel {
+import 'package:hive/hive.dart';
+
+
+part 'note_model.g.dart';
+@HiveType(typeId: 0)
+class NoteModel extends HiveObject {
+  @HiveField(0)
   final String title;
-  final String date;
+  @HiveField(1)
   final String subtitle;
+  @HiveField(2)
+  final String date;
+  @HiveField(3)
   final int color;
 
-
-  NoteModel({required this.title, required this.date, required this.subtitle, required this.color});
+  NoteModel(
+      {required this.title,
+      required this.date,
+      required this.subtitle,
+      required this.color});
 }
